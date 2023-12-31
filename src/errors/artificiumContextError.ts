cd38103d4d8810b1554f0e8f2a5e8cae3ba133fc
frex-is <muller.je@outlook.fr>
2023-12-31 01:33:31 +0100

    📝 add documentation ArtificiumContextError

--- src/errors/artificiumContextError.ts
+++ src/errors/artificiumContextError.ts
@@ -1,3 +1,11 @@
+/**
+ * @class
+ * Custom error class for use with the Artificium context.
+ *
+ * This error is thrown when attempting to access the Artificium context without it being set, typically when using the `useArtificium` hook outside the context of an `ArtificiumProvider`.
+ *
+ * @extends Error
+ */
 class ArtificiumContextError extends Error {
   constructor() {
     super("useArtificium must be used within an ArtificiumProvider")
