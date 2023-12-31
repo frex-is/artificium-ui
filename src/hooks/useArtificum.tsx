91819e6ee2126217a397cac8d9e63a10e15e2e77
frex-is <muller.je@outlook.fr>
2023-12-31 01:28:47 +0100

    📝 add useArtificium documentation

--- src/hooks/useArtificum.tsx
+++ src/hooks/useArtificum.tsx
@@ -1,6 +1,20 @@
 import { useContext } from "react";
 import { ArtificiumContext } from "../providers/artificium/artificium";
 
+/**
+ * Custom hook for accessing the Artificium context.
+ *
+ * @throws {ArtificiumContextError} Throws an error if the Artificium context is not available.
+ * 
+ * @returns {ArtificiumContext} The Artificium context.
+ * @example
+ * ```jsx
+ * const MyComponent = () => {
+ *   const artificiumContext = useArtificium();
+ *   // Use artificiumContext here...
+ * };
+ * ```
+ */
 export const useArtificium = () => {
   const context = useContext(ArtificiumContext);
 
