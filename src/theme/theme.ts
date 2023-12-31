a210310e922d6a016d920663b59c074bbf5351b3
frex-is <muller.je@outlook.fr>
2023-12-31 01:21:59 +0100

    💄add theme system

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+export interface ArtificiumTheme {
+  color: {
+    primary: string
+  },
+  font?: {
+    primary: string
+  }
+}
+
+export const darkTheme = {
+  color: {
+    primary: "red"
+  }
+}
