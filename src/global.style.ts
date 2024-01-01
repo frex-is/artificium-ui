4771bc3edb0c09c7913e376c501a1f491d8f2fa2
frex-is <muller.je@outlook.fr>
2024-01-01 16:55:38 +0100

    💄add font

--- src/global.style.ts
+++ src/global.style.ts
@@ -1,5 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
+// @ts-ignore
+import JakartaFont from "../assets/font/PlusJakartaSans-SemiBold.ttf"
+
 export const GlobalStyle = createGlobalStyle`
-  
+  @font-face {
+    font-family: 'jakarta';
+    src: url(${JakartaFont}) format('truetype');
+  }
 `;
