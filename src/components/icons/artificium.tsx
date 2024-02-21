bc36a2ef1a9b45233b7e4a4ecc25ba5c3a2bea7d
frex-is <muller.je@outlook.fr>
2024-02-21 19:34:42 +0100

    � forget viewbox icon

--- src/components/icons/artificium.tsx
+++ src/components/icons/artificium.tsx
@@ -3,10 +3,10 @@ import { SVGProps } from "react";
 
 export const ArtificiumIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
+    viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     width={24}
     height={24}
-    preserveAspectRatio="xMidYMid meet"
     fill="none"
     {...props}
   >
