59bb6a09f8e1b737dec6a1590032f57dfb320f2d
frex-is <muller.je@outlook.fr>
2024-02-28 23:30:21 +0100

    🚧 work in progress typographie

--- src/stories/typography/typography.stories.tsx
+++ src/stories/typography/typography.stories.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { ArtificiumProvider } from "../../providers";
 import { StoryCard } from "../utils/story-card/story-card";
 import {
+  Circle,
   FontNameContainer,
+  FontTypeContainer,
   HeadingContainer,
   IconTypography,
   InformationContainer,
@@ -46,43 +48,160 @@ const Information = ({ title, value }: { title: string; value: string }) => {
 const TypographyCard = () => {
   const { theme } = useArtificium();
   const fontWeights: FontWeight[] = ["regular", "medium", "semi-bold", "bold"];
+  const sections: any[] = [
+    {
+      size: "xl",
+      information: {
+        size: "36px",
+        lineHeight: "44px",
+        letterSpacing: "0",
+      },
+    },
+    {
+      size: "l",
+      information: {
+        size: "32px",
+        lineHeight: "40px",
+        letterSpacing: "0",
+      },
+    },
+    {
+      size: "m",
+      information: {
+        size: "28px",
+        lineHeight: "36px",
+        letterSpacing: "0",
+      },
+    },
+    {
+      size: "s",
+      information: {
+        size: "24px",
+        lineHeight: "32px",
+        letterSpacing: "0",
+      },
+    },
+    {
+      size: "xs",
+      information: {
+        size: "20px",
+        lineHeight: "28px",
+        letterSpacing: "0",
+      },
+    },
+  ];
 
   return (
     <StoryCard title="Typography">
       <SubHeader theme={theme}>
-        <FontNameContainer theme={theme}>hello</FontNameContainer>
+        <FontNameContainer theme={theme}>
+          <Heading
+            size="xl"
+            fontWeight="semi-bold"
+          >
+            Plus Jakarta Sans
+          </Heading>
+          <FontTypeContainer>
+            <Heading
+              size="xs"
+              fontWeight="bold"
+            >
+              Bold
+            </Heading>
+            <Circle color={theme.color.primary[400]} />
+            <Heading
+              size="xs"
+              fontWeight="semi-bold"
+            >
+              SemiBold
+            </Heading>
+            <Circle color={theme.color.primary[400]} />
+            <Heading
+              size="xs"
+              fontWeight="medium"
+            >
+              Medium
+            </Heading>
+            <Circle color={theme.color.primary[400]} />
+            <Heading
+              size="xs"
+              fontWeight="regular"
+            >
+              Regular
+            </Heading>
+          </FontTypeContainer>
+        </FontNameContainer>
         <IconTypography theme={theme}>Aa</IconTypography>
       </SubHeader>
-      <TypographyContainer>
-        <HeadingContainer>
-          <Section theme={theme}>
-            <SectionTitle>
-              {fontWeights.map(fontWeight => (
-                <Heading
-                  size="xl"
-                  fontWeight={fontWeight}
-                >
-                  Heading XL
-                </Heading>
-              ))}
-            </SectionTitle>
-            <InformationSection>
-              <Information
-                title="Size"
-                value="36px"
-              />
-              <Information
-                title="Line Height"
-                value="44px"
-              />
-              <Information
-                title="Letter Spacing"
-                value="0px"
-              />
-            </InformationSection>
-          </Section>
+      <TypographyContainer backgroundColor={theme.color.primary[800]}>
+        <HeadingContainer borderColor={theme.color.primary[600]}>
+          {sections.map((section, index) => {
+            const isLastSection = index === sections.length - 1;
+
+            return (
+              <Section borderColor={isLastSection ? "none" : theme.color.primary[600]}>
+                <SectionTitle>
+                  {fontWeights.map(fontWeight => (
+                    <Heading
+                      size={section.size}
+                      fontWeight={fontWeight}
+                    >
+                      Heading {section.size.toUpperCase()}
+                    </Heading>
+                  ))}
+                </SectionTitle>
+                <InformationSection>
+                  <Information
+                    title="Size"
+                    value={section.information.size}
+                  />
+                  <Information
+                    title="Line Height"
+                    value={section.information.lineHeight}
+                  />
+                  <Information
+                    title="Letter Spacing"
+                    value={section.information.letterSpacing}
+                  />
+                </InformationSection>
+              </Section>
+            );
+          })}
         </HeadingContainer>
-        <ParagraphContainer></ParagraphContainer>
+        <ParagraphContainer>
+          {sections.map((section, index) => {
+            const isLastSection = index === sections.length - 1;
+
+            return (
+              <Section borderColor={isLastSection ? "none" : theme.color.primary[600]}>
+                <SectionTitle>
+                  {fontWeights.map(fontWeight => (
+                    <Paragraph
+                      size={section.size}
+                      fontWeight={fontWeight}
+                    >
+                      Body {section.size.toUpperCase()}
+                    </Paragraph>
+                  ))}
+                </SectionTitle>
+                <InformationSection>
+                  <Information
+                    title="Size"
+                    value={section.information.size}
+                  />
+                  <Information
+                    title="Line Height"
+                    value={section.information.lineHeight}
+                  />
+                  <Information
+                    title="Letter Spacing"
+                    value={section.information.letterSpacing}
+                  />
+                </InformationSection>
+              </Section>
+            );
+          })}
+        </ParagraphContainer>
       </TypographyContainer>
     </StoryCard>
   );
--- src/stories/typography/typography.style.ts
+++ src/stories/typography/typography.style.ts
@@ -10,6 +10,9 @@ export const SubHeader = styled.div`
 
 export const FontNameContainer = styled.div`
   color: ${({ theme }) => theme.color.primary[0]};
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 
 export const IconTypography = styled.div`
@@ -20,24 +23,32 @@ export const IconTypography = styled.div`
   background: -webkit-linear-gradient(${({ theme }) => theme.gradient.secondary});
 `;
 
-export const TypographyContainer = styled.div`
+export const TypographyContainer = styled.div<{
+  backgroundColor: string;
+}>`
   display: flex;
+  background-color: ${({ backgroundColor }) => backgroundColor};
 `;
 
-export const HeadingContainer = styled.div`
+export const HeadingContainer = styled.div<{
+  borderColor: string;
+}>`
   padding: 16px 64px;
+  border-right: 1px solid ${({ borderColor }) => borderColor};
 `;
 
 export const ParagraphContainer = styled.div`
   padding: 16px 64px;
 `;
 
-export const Section = styled.div`
+export const Section = styled.div<{
+  borderColor: string;
+}>`
   display: flex;
   flex-direction: column;
   gap: 16px;
   padding: 48px 0;
-  border-bottom: 1px solid ${({ theme }) => theme.color.primary[600]};
+  border-bottom: 1px solid ${({ borderColor }) => borderColor};
 `;
 
 export const SectionTitle = styled.div`
@@ -51,3 +62,18 @@ export const InformationSection = styled.div`
 `;
 
 export const InformationContainer = styled.div``;
+
+export const FontTypeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const Circle = styled.div<{
+  color: string;
+}>`
+  width: 4px;
+  height: 4px;
+  background-color: ${({ color }) => color};
+  border-radius: 100%;
+`;
