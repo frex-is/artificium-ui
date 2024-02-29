e18d316705f699b6a13b119a2972b68eb7092bf1
frex-is <muller.je@outlook.fr>
2024-02-29 12:57:42 +0100

    📝add documentation heading

--- src/components/typography/heading/heading.tsx
+++ src/components/typography/heading/heading.tsx
@@ -5,17 +5,29 @@ import { FontWeight } from "../../../types/fontWeight";
 import { useHeading } from "./useHeading";
 
 export interface HeadingProps {
-  size: Size;
   children: ReactNode;
-  fontWeight?: FontWeight;
+  size?: Size;
   color?: string;
+  fontWeight?: FontWeight;
 }
 
+/**
+ * Represents a heading component with customizable size, font weight, and color.
+ *
+ * @component
+ * @param props `HeadingProps` - The properties of the Heading component.
+ * @param props.children `ReactNode` - The children elements to be rendered within the heading.
+ * @param props.size `Size`The size of the heading. Possible value are 'xs', 's', 'm', 'l', 'xl'.
+ * @param props.fontWeight `Optional<FontWeight>` - The font weight of the heading. Possible value are 'regular', 'medium', 'semi-bold', 'bold'.
+ * @param props.color `Optional<string>` - The color of the heading. Optional, defaults to undefined.
+ *
+ * @returns {JSX.Element} - Return a JSX element  representing the heading.
+ */
 export const Heading: FunctionalComponent<HeadingProps> = ({
-  size,
   children,
-  fontWeight = "regular",
+  size = "m",
   color = undefined,
+  fontWeight = "regular",
 }) => {
   const { fontFamily, fontSize, lineHeight, letterSpacing, textColor } = useHeading({
     size,
